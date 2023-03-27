23bc4eddfd58cc4044edadaaf6be9e08800558fc
Thando <thandomsh8@gmail.com>
2023-03-27 16:32:07 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+void swap_int(int *a);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void _puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char _string(char *const);
+
+int _atoi(char *s);
